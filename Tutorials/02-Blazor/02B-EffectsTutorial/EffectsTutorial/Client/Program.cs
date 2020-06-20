e9ad8b6224bdc02e7f3d3e9ccee03b5e110f1ab1
Peter Morris <mrpmorris@gmail.com>
2020-06-20 10:06:54 +0100

    Blazor effects

--- Tutorials/02-Blazor/02B-EffectsTutorial/EffectsTutorial/Client/Program.cs
+++ Tutorials/02-Blazor/02B-EffectsTutorial/EffectsTutorial/Client/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Fluxor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FluxorBlazorWeb.EffectsTutorial.Client
 {
@@ -10,6 +13,9 @@ namespace FluxorBlazorWeb.EffectsTutorial.Client
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("app");
+
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 			builder.Services.AddFluxor(o => o
 				.ScanAssemblies(typeof(Program).Assembly)
 			);
--- Tutorials/02-Blazor/02B-EffectsTutorial/EffectsTutorial/Server/Startup.cs
+++ Tutorials/02-Blazor/02B-EffectsTutorial/EffectsTutorial/Server/Startup.cs
@@ -31,6 +31,12 @@ namespace FluxorBlazorWeb.EffectsTutorial.Server
 				app.UseDeveloperExceptionPage();
 				app.UseWebAssemblyDebugging();
 			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
 			app.UseStaticFiles();
 			app.UseBlazorFrameworkFiles();
