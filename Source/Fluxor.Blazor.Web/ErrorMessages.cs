800dee972816591f2dca3d0a704a5c1d9f508e11
Peter Morris <mrpmorris@gmail.com>
2020-08-26 21:05:16 +0100

    Release/3.7 (#91)
    * Allow observer callback code to unsubscribe from action notifications (Fixes #84)
    
    * Throw exception if base.OnInitialzed not called (Fixes #82)
    
    * Remove unneeded reflection (Fixes #87) (#90)
    
    * Include ActionSubscriber tutorial in solution

---
+++ Source/Fluxor.Blazor.Web/ErrorMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Fluxor.Blazor.Web
+{
+	internal class ErrorMessages
+	{
+		internal const string ForgottenToCallBaseOnInitialized =
+			"Have you forgotten to call base.OnInitialized() in your component?";
+	}
+}
