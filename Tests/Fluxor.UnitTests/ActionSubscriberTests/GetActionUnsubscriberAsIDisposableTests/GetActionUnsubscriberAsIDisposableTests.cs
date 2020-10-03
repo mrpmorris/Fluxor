175cc4e7c51be47e54c2a06c79c1ab667da1cd11
Peter Morris <mrpmorris@gmail.com>
2020-10-03 15:13:53 +0100

    GetActionUnsubscriberAsIDisposableTests

--- Tests/Fluxor.UnitTests/ActionSubscriberTests/GetActionUnsubscriberAsIDisposableTests/GetActionUnsubscriberAsIDisposableTests.cs
+++ Tests/Fluxor.UnitTests/ActionSubscriberTests/GetActionUnsubscriberAsIDisposableTests/GetActionUnsubscriberAsIDisposableTests.cs
@@ -1,8 +1,8 @@
-﻿using Fluxor.UnitTests.SupportFiles;
+﻿using Fluxor.UnitTests.ActionSubscriberTests.GetActionUnsubscriberAsIDisposableTests.SupportFiles;
 using System;
 using Xunit;
 
-namespace Fluxor.UnitTests.ActionSubscriberTests
+namespace Fluxor.UnitTests.ActionSubscriberTests.GetActionUnsubscriberAsIDisposableTests
 {
 	public class GetActionUnsubscriberAsIDisposableTests
 	{
