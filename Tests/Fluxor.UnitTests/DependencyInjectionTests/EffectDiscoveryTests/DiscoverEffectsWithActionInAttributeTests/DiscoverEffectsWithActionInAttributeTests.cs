f84d1534bf664abc3875f300b578f0b14d602492
Peter Morris <mrpmorris@gmail.com>
2020-10-04 17:07:05 +0100

    Tests for effects with action in attribute

---
+++ Tests/Fluxor.UnitTests/DependencyInjectionTests/EffectDiscoveryTests/DiscoverEffectsWithActionInAttributeTests/DiscoverEffectsWithActionInAttributeTests.cs
@@ -0,0 +1,38 @@
+﻿using Fluxor.UnitTests.DependencyInjectionTests.EffectDiscoveryTests.DiscoverEffectsWithActionInAttributeTests.SupportFiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Fluxor.UnitTests.DependencyInjectionTests.EffectDiscoveryTests.DiscoverEffectsWithActionInAttributeTests
+{
+	public class DiscoverEffectsWithActionInAttributeTests
+	{
+		private readonly IServiceProvider ServiceProvider;
+		private readonly IStore Store;
+		private readonly InvokeCountService InvokeCountService;
+
+		[Fact]
+		public void WhenActionIsDispatched_ThenEffectWithActionInMethodSignatureIsExecuted()
+		{
+			Assert.Equal(0, InvokeCountService.GetCount());
+			Store.Dispatch(new TestAction());
+			Assert.Equal(1, InvokeCountService.GetCount());
+		}
+
+		public DiscoverEffectsWithActionInAttributeTests()
+		{
+			InvokeCountService = new InvokeCountService();
+
+			var services = new ServiceCollection();
+			services.AddScoped(_ => InvokeCountService);
+			services.AddFluxor(x => x
+				.ScanAssemblies(GetType().Assembly)
+				.AddMiddleware<IsolatedTests>());
+
+			ServiceProvider = services.BuildServiceProvider();
+			Store = ServiceProvider.GetRequiredService<IStore>();
+
+			Store.InitializeAsync().Wait();
+		}
+	}
+}
---
+++ Tests/Fluxor.UnitTests/DependencyInjectionTests/EffectDiscoveryTests/DiscoverEffectsWithActionInAttributeTests/SupportFiles/InvokeCountService.cs
@@ -0,0 +1,13 @@
+﻿namespace Fluxor.UnitTests.DependencyInjectionTests.EffectDiscoveryTests.DiscoverEffectsWithActionInAttributeTests.SupportFiles
+{
+	public class InvokeCountService
+	{
+		private int Count;
+
+		public int GetCount() => Count;
+		public void IncrementCount()
+		{
+			Count++;
+		}
+	}
+}
