08d32623d9ecaa43b7e3d32b2fb569b9c816d5aa
Peter Morris <mrpmorris@gmail.com>
2020-10-11 11:54:18 +0100

    Test generic effect class

---
+++ Tests/Fluxor.UnitTests/DependencyInjectionTests/EffectDiscoveryTests/DiscoverGenericEffectClassesTests/SupportFiles/TestGenericEffectClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace Fluxor.UnitTests.DependencyInjectionTests.EffectDiscoveryTests.DiscoverGenericEffectClassesTests.SupportFiles
+{
+	public class TestGenericEffectClass : AbstractTestGenericEffectClass<TestAction>
+	{
+		public TestGenericEffectClass(InvokeCountService invokeCountService) : base(invokeCountService)
+		{
+		}
+	}
+
+	public abstract class AbstractTestGenericEffectClass<T> : Effect<T>
+	{
+		private readonly InvokeCountService InvokeCountService;
+
+		protected AbstractTestGenericEffectClass(InvokeCountService invokeCountService)
+		{
+			InvokeCountService = invokeCountService;
+		}
+
+		protected override Task HandleAsync(T action, IDispatcher dispatcher)
+		{
+			InvokeCountService.IncrementCount();
+			return Task.CompletedTask;
+		}
+	}
+}
