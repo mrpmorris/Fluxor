977c9d080d6937878ded5b3e441f1ea342d65be8
Peter Morris <mrpmorris@gmail.com>
2020-10-11 12:03:51 +0100

    Test generic reducer classes

---
+++ Tests/Fluxor.UnitTests/DependencyInjectionTests/ReducerDiscoveryTests/DiscoverGenericReducerClassesTests/SupportFiles/TestState.cs
@@ -0,0 +1,12 @@
+﻿namespace Fluxor.UnitTests.DependencyInjectionTests.ReducerDiscoveryTests.DiscoverGenericReducerClassesTests.SupportFiles
+{
+	public class TestState
+	{
+		public bool ReducerWasExecuted { get; private set; }
+
+		public TestState(bool reducerWasExecuted)
+		{
+			ReducerWasExecuted = reducerWasExecuted;
+		}
+	}
+}
