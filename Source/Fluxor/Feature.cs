131c23bf481891829b87d7fa407c1b6a35b17ed9
Peter Morris <mrpmorris@gmail.com>
2020-10-16 10:21:23 +0100

    WIP

--- Source/Fluxor/Feature.cs
+++ Source/Fluxor/Feature.cs
@@ -27,12 +27,17 @@ namespace Fluxor
 		/// <returns>The initial state</returns>
 		protected abstract TState GetInitialState();
 
+		protected virtual byte GetMaximumNotificationsPerSecond() => 0;
+
 		/// <summary>
 		/// A list of reducers registered with this feature
 		/// </summary>
 		protected readonly List<IReducer<TState>> Reducers = new List<IReducer<TState>>();
 
 		private SpinLock SpinLock = new SpinLock();
+		private Timer NotificationTimer;
+		private int NotificationThrottleWindowMs;
+		private DateTime LastNotificationTime;
 
 		/// <summary>
 		/// Creates a new instance
@@ -40,6 +45,9 @@ namespace Fluxor
 		public Feature()
 		{
 			State = GetInitialState();
+			byte frameRate = GetMaximumNotificationsPerSecond();
+			if (frameRate > 0)
+				NotificationThrottleWindowMs = 1000 / frameRate;
 		}
 
 		private EventHandler untypedStateChanged;
@@ -81,10 +89,13 @@ namespace Fluxor
 			get => _State;
 			protected set
 			{
-				bool stateHasChanged = !Object.ReferenceEquals(_State, value);
-				_State = value;
-				if (stateHasChanged)
-					TriggerStateChangedCallbacks(value);
+				SpinLock.ExecuteLocked(() =>
+				{
+					bool stateHasChanged = !Object.ReferenceEquals(_State, value);
+					_State = value;
+					if (stateHasChanged)
+						TriggerStateChangedCallbacks();
+				});
 			}
 		}
 
@@ -111,9 +122,46 @@ namespace Fluxor
 			State = newState;
 		}
 
-		private void TriggerStateChangedCallbacks(TState newState)
+		private void TriggerStateChangedCallbacks()
 		{
-			stateChanged?.Invoke(this, newState);
+			// If no throttling then notify immediately
+			if (NotificationThrottleWindowMs == 0)
+			{
+				OnStateChanged();
+				return;
+			}
+
+			int timeSinceLastNotificationMs =
+				(int)(DateTime.UtcNow - LastNotificationTime).TotalMilliseconds;
+
+			// If we are outside the render throttle window then notify immediately
+			if (timeSinceLastNotificationMs > NotificationThrottleWindowMs)
+			{
+				OnStateChanged();
+				return;
+			}
+
+			// If waiting for a previously throttled notification to execute
+			// then ignore this notification request
+			if (NotificationTimer != null)
+				return;
+
+			// This is the first time we've been told to notify observers
+			// within the throttle window, so stop accepting requests to
+			// notify observers until the window has expired, and set
+			// a timer to trigger a notification at the end of that window
+			// so we get a single notification at the end
+			NotificationTimer = new Timer(
+				callback: _ => OnStateChanged(),
+				state: null,
+				dueTime: NotificationThrottleWindowMs - timeSinceLastNotificationMs,
+				period: 0);
+		}
+
+		private void OnStateChanged()
+		{
+			LastNotificationTime = DateTime.UtcNow;
+			stateChanged?.Invoke(this, State);
 			untypedStateChanged?.Invoke(this, EventArgs.Empty);
 		}
 	}
