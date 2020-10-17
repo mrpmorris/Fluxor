1806ff0d29bbb8e278faa8dca92098c7b70fd4b6
Peter Morris <mrpmorris@gmail.com>
2020-10-17 15:39:30 +0100

    Done

--- Source/Fluxor/Feature.cs
+++ Source/Fluxor/Feature.cs
@@ -1,4 +1,5 @@
 ﻿using Fluxor.Extensions;
+using Fluxor.UnsupportedClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,9 @@ namespace Fluxor
 	/// <see cref="IFeature{TState}"/>
 	public abstract class Feature<TState> : IFeature<TState>
 	{
+		/// <see cref="IFeature.MaximumStateChangedNotificationsPerSecond"/>
+		public byte MaximumStateChangedNotificationsPerSecond { get; set; }
+
 		/// <see cref="IFeature.GetName"/>
 		public abstract string GetName();
 
@@ -27,27 +31,21 @@ namespace Fluxor
 		/// <returns>The initial state</returns>
 		protected abstract TState GetInitialState();
 
-		protected virtual byte GetMaximumNotificationsPerSecond() => 0;
-
 		/// <summary>
 		/// A list of reducers registered with this feature
 		/// </summary>
 		protected readonly List<IReducer<TState>> Reducers = new List<IReducer<TState>>();
 
 		private SpinLock SpinLock = new SpinLock();
-		private Timer NotificationTimer;
-		private int NotificationThrottleWindowMs;
-		private DateTime LastNotificationTime;
+		private ThrottledInvoker TriggerStateChangedCallbacksThrottler;
 
 		/// <summary>
 		/// Creates a new instance
 		/// </summary>
 		public Feature()
 		{
+			TriggerStateChangedCallbacksThrottler = new ThrottledInvoker(TriggerStateChangedCallbacks);
 			State = GetInitialState();
-			byte frameRate = GetMaximumNotificationsPerSecond();
-			if (frameRate > 0)
-				NotificationThrottleWindowMs = 1000 / frameRate;
 		}
 
 		private EventHandler untypedStateChanged;
@@ -94,7 +92,7 @@ namespace Fluxor
 					bool stateHasChanged = !Object.ReferenceEquals(_State, value);
 					_State = value;
 					if (stateHasChanged)
-						TriggerStateChangedCallbacks();
+						TriggerStateChangedCallbacksThrottler.Invoke(MaximumStateChangedNotificationsPerSecond);
 				});
 			}
 		}
@@ -124,45 +122,6 @@ namespace Fluxor
 
 		private void TriggerStateChangedCallbacks()
 		{
-			// If no throttling then notify immediately
-			if (NotificationThrottleWindowMs == 0)
-			{
-				OnStateChanged();
-				return;
-			}
-
-			// If waiting for a previously throttled notification to execute
-			// then ignore this notification request
-			if (NotificationTimer != null)
-				return;
-
-			int timeSinceLastNotificationMs =
-				(int)(DateTime.UtcNow - LastNotificationTime).TotalMilliseconds;
-
-			// If we are outside the render throttle window then notify immediately
-			if (timeSinceLastNotificationMs > NotificationThrottleWindowMs)
-			{
-				OnStateChanged();
-				return;
-			}
-
-			// This is the first time we've been told to notify observers
-			// within the throttle window, so stop accepting requests to
-			// notify observers until the window has expired, and set
-			// a timer to trigger a notification at the end of that window
-			// so we get a single notification at the end
-			NotificationTimer = new Timer(
-				callback: _ => OnStateChanged(),
-				state: null,
-				dueTime: NotificationThrottleWindowMs - timeSinceLastNotificationMs,
-				period: 0);
-		}
-
-		private void OnStateChanged()
-		{
-			NotificationTimer?.Dispose();
-			NotificationTimer = null;
-			LastNotificationTime = DateTime.UtcNow;
 			stateChanged?.Invoke(this, State);
 			untypedStateChanged?.Invoke(this, EventArgs.Empty);
 		}
---
+++ Tests/Fluxor.UnitTests/UnsupportedClassesTests/ThrottledInvokerTests/InvokeTests.cs
@@ -0,0 +1,124 @@
+﻿using Fluxor.UnsupportedClasses;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fluxor.UnitTests.UnsupportedClassesTests.ThrottledInvokerTests
+{
+	public class InvokeTests
+	{
+		private ThrottledInvoker Subject;
+		private int InvokeCount;
+		private Action ActionToExecute;
+
+		public InvokeTests()
+		{
+			ActionToExecute = () => Interlocked.Increment(ref InvokeCount);
+			Subject = new ThrottledInvoker(() => ActionToExecute());
+		}
+
+		[Fact]
+		public void WhenNotThrottled_ThenInvokesImmediately()
+		{
+			for (int i = 0; i < 42; i++)
+				Subject.Invoke();
+
+			Assert.Equal(42, InvokeCount);
+		}
+
+		[Fact]
+		public void WhenInvokedInsideThrottleWindow_ThenDoesntInvoke()
+		{
+			Subject.ThrottleWindowMs = 2000;
+
+			for (int i = 0; i < 42; i++)
+				Subject.Invoke();
+
+			Assert.Equal(1, InvokeCount);
+		}
+
+		[Fact]
+		public async Task WhenInvokedOutsideThrottleWindow_ThenInvokesImmediately()
+		{
+			Subject.ThrottleWindowMs = 50;
+			Subject.Invoke();
+			await Task.Delay(50);
+			Subject.Invoke();
+
+			Assert.Equal(2, InvokeCount);
+		}
+
+		[Fact]
+		public async Task WhenExecutedByMultipleThreads_ThenThrottlesSuccessfully()
+		{
+			int processorCount = Environment.ProcessorCount;
+
+			ManualResetEvent[] threadReadyEvents =
+				Enumerable.Range(0, processorCount)
+				.Select(x => new ManualResetEvent(false))
+				.ToArray();
+
+			ManualResetEvent[] threadCompletedEvents =
+				Enumerable.Range(0, processorCount)
+				.Select(x => new ManualResetEvent(false))
+				.ToArray();
+
+			bool testCompleted = false;
+			var startTestEvent = new ManualResetEvent(false);
+			Subject.ThrottleWindowMs = 100;
+
+			for (int i = 0; i < processorCount; i++)
+			{
+				ManualResetEvent threadReadyEvent = threadReadyEvents[i];
+				ManualResetEvent threadCompletedEvent = threadCompletedEvents[i];
+				new Thread(_ => 
+				{
+					threadReadyEvent.Set();
+					startTestEvent.WaitOne();
+					while (!testCompleted)
+						Subject.Invoke();
+					threadCompletedEvent.Set();
+				}).Start();
+			}
+
+			// Wait for all threads to be ready
+			WaitHandle.WaitAll(threadReadyEvents);
+
+			// Allow a large window for the first invoke
+			int failCount = 0;
+			var lastInvokeTime = DateTime.UtcNow.AddDays(-1);
+			int smallestFailTime = int.MaxValue;
+
+			// Set up the action to execute
+			// This ensures each call is on or outside the MS window
+			ActionToExecute = () =>
+			{
+				long elapsedMs = (int)(DateTime.UtcNow - lastInvokeTime).TotalMilliseconds;
+				if (elapsedMs < Subject.ThrottleWindowMs)
+				{
+					Interlocked.Increment(ref failCount);
+					smallestFailTime = (int)Math.Min(smallestFailTime, elapsedMs);
+				}
+				lastInvokeTime = DateTime.UtcNow;
+			};
+
+			// Start the test
+			startTestEvent.Set();
+
+			await Task.Delay(Subject.ThrottleWindowMs * 4);
+
+			testCompleted = true;
+
+			// Wait for all threads to finish
+			WaitHandle.WaitAll(threadCompletedEvents);
+
+			if (failCount > 0)
+				Assert.True(false,
+					$"Failed: Smallest elapsed time was {smallestFailTime}" +
+					$" when it should be {Subject.ThrottleWindowMs}" +
+					$" failed {failCount} times.");
+		}
+	}
+}
