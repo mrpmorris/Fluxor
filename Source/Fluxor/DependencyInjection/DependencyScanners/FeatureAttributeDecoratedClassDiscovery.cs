90be5ecd91826e87d577eeae671c687d0bbea432
Peter Morris <mrpmorris@gmail.com>
2021-10-24 10:13:31 +0100

    Start feature attribute discovery

---
+++ Source/Fluxor/DependencyInjection/DependencyScanners/FeatureAttributeDecoratedClassDiscovery.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Fluxor.DependencyInjection.DependencyScanners
+{
+	internal class FeatureAttributeDecoratedClassDiscovery
+	{
+		internal static DiscoveredFeatureAttributeDecoratedClass[] DiscoverFeatureAttributeDecoratedClasses(
+			IServiceCollection serviceCollection, IEnumerable<Type> allCandidateTypes)
+		{
+			(Type stateType, FeatureAttribute attr)[] discoveredClasses = allCandidateTypes
+				.Select(x => new
+				{
+					Type = x,
+					FeatureAttribute = x.GetCustomAttribute<FeatureAttribute>()
+				})
+				.Where(x => x.FeatureAttribute != null)
+				.Select(x => (x.Type, x.FeatureAttribute))
+				.ToArray();
+
+			foreach (var item in discoveredClasses)
+			{
+				//TODO: Resister
+			}
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Source/Fluxor/DependencyInjection/DiscoveredFeatureAttributeDecoratedClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Fluxor.DependencyInjection
+{
+	internal class DiscoveredFeatureAttributeDecoratedClass
+	{
+		public readonly Type StateType;
+		public readonly Type FeatureInterfaceGenericType;
+		public readonly Func<IFeature> CreateFeature;
+
+		public DiscoveredFeatureAttributeDecoratedClass(
+			FeatureAttribute featureAttribute,
+			Type stateType)
+		{
+			if (featureAttribute == null)
+				throw new ArgumentNullException(nameof(featureAttribute));
+			if (stateType == null)
+				throw new ArgumentNullException(nameof(stateType));
+
+			StateType = stateType;
+			FeatureInterfaceGenericType = typeof(IFeature<>).MakeGenericType(stateType);
+
+			if (featureAttribute.CreateInitialStateMethodName == null)
+				CreateFeature = CreateParameterlessConstructorFactory(featureAttribute);
+			else
+				throw new NotImplementedException(nameof(FeatureAttribute.CreateInitialStateMethodName));
+		}
+
+		private static T CreateStateUsingParameterlessConstructor<T>()
+			where T : new()
+			=> new T();
+
+		private Func<IFeature> CreateParameterlessConstructorFactory(FeatureAttribute featureAttribute)
+		{
+			ConstructorInfo constructor = StateType.GetConstructor(Array.Empty<Type>());
+			if (constructor == null)
+				ThrowConstructorException();
+
+			MethodInfo createMethod = typeof(DiscoveredFeatureAttributeDecoratedClass)
+				.GetMethod(
+					nameof(CreateStateUsingParameterlessConstructor),
+					BindingFlags.NonPublic | BindingFlags.Static)
+				.MakeGenericMethod(StateType);
+
+			return (Func<IFeature>)createMethod.CreateDelegate(typeof(Func<IFeature>));
+		}
+
+		private void ThrowConstructorException()
+		{
+			throw new ArgumentException(
+				message: $"{StateType.Name} must implement a public parameterless constructor if" +
+				$" {nameof(FeatureAttribute)}.{nameof(FeatureAttribute.CreateInitialStateMethodName)} is null");
+		}
+	}
+}
