8f4e3b794d3e1757efb86ba6a05f50d5298af773
Peter Morris <mrpmorris@gmail.com>
2022-03-10 23:49:54 +0000

    Remove IDisposable from IStateSubscriber #271

--- Source/Fluxor/StateSubscriber.cs
+++ Source/Fluxor/StateSubscriber.cs
@@ -3,28 +3,28 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using GetStateDelegate = System.Func<object, Fluxor.IStateChangedNotifier>;
+using GetStateChangedPropertyDelegate = System.Func<object, Fluxor.IStateChangedNotifier>;
 
 namespace Fluxor
 {
 	/// <summary>
-	/// A utility class that automaticaly subscribes to all <see cref="IState{TState}"/> properties
+	/// A utility class that automaticaly subscribes to all <see cref="IStateChangedNotifier"/> properties
 	/// on a specific object
 	/// </summary>
 	public static class StateSubscriber
 	{
-		private static readonly ConcurrentDictionary<Type, IEnumerable<GetStateDelegate>> ValueDelegatesByType;
+		private static readonly ConcurrentDictionary<Type, IEnumerable<GetStateChangedPropertyDelegate>> ValueDelegatesByType;
 
 		static StateSubscriber()
 		{
-			ValueDelegatesByType = new ConcurrentDictionary<Type, IEnumerable<GetStateDelegate>>();
+			ValueDelegatesByType = new ConcurrentDictionary<Type, IEnumerable<GetStateChangedPropertyDelegate>>();
 		}
 
 		/// <summary>
-		/// Subscribes to all <see cref="IState{TState}"/> properties on the specified <paramref name="subject"/>
+		/// Subscribes to all <see cref="IStateChangedNotifier"/> properties on the specified <paramref name="subject"/>
 		/// to ensure <paramref name="callback"/> is called whenever a state is modified
 		/// </summary>
-		/// <param name="subject">The object to scan for <see cref="IState{TState}"/> properties.</param>
+		/// <param name="subject">The object to scan for <see cref="IStateChangedNotifier"/> properties.</param>
 		/// <param name="callback">The action to execute when one of the states are modified</param>
 		/// <returns></returns>
 		public static IDisposable Subscribe(object subject, Action<IStateChangedNotifier> callback)
@@ -34,49 +34,50 @@ namespace Fluxor
 			if (callback is null)
 				throw new ArgumentNullException(nameof(callback));
 
-			IEnumerable<GetStateDelegate> getStateDelegates = GetStateDelegatesForType(subject.GetType());
-			var subscriptions = new List<(IStateChangedNotifier State, EventHandler Handler)>();
-			foreach (GetStateDelegate getState in getStateDelegates)
+			IEnumerable<GetStateChangedPropertyDelegate> getStateChangedNotifierPropertyDelegates =
+				GetStateChangedNotifierPropertyDelegatesForType(subject.GetType());
+			var subscriptions = new List<(IStateChangedNotifier StateChangedNotifier, EventHandler Handler)>();
+			
+			foreach (GetStateChangedPropertyDelegate getStateChangedNotifierPropertyValue in getStateChangedNotifierPropertyDelegates)
 			{
-				var state = (IStateChangedNotifier)getState(subject);
-				var handler = new EventHandler((s, a) => callback(state));
+				IStateChangedNotifier stateChangedNotifier = getStateChangedNotifierPropertyValue(subject);
+				var handler = new EventHandler((s, a) => callback(stateChangedNotifier));
 
-				subscriptions.Add((state, handler));
-				state.StateChanged += handler;
+				subscriptions.Add((stateChangedNotifier, handler));
+				stateChangedNotifier.StateChanged += handler;
 			}
+
 			return new DisposableCallback(
 				id: $"{nameof(StateSubscriber)}.{nameof(Subscribe)}",
 				() =>
 					{
 						foreach (var subscription in subscriptions)
-							subscription.State.StateChanged -= subscription.Handler;
+							subscription.StateChangedNotifier.StateChanged -= subscription.Handler;
 					});
 		}
 
-		private static IEnumerable<PropertyInfo> GetStateProperties(Type t) =>
+		private static IEnumerable<PropertyInfo> GetStateChangedNotifierProperties(Type t) =>
 			t == typeof(object)
 			? Enumerable.Empty<PropertyInfo>()
-			: GetStateProperties(t.BaseType)
+			: GetStateChangedNotifierProperties(t.BaseType)
 				.Union(
 					t.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly)
-						.Where(p => p.PropertyType.IsGenericType)
-						.Where(p => p.PropertyType.GetGenericTypeDefinition() == typeof(IState<>))
+						.Where(p => typeof(IStateChangedNotifier).IsAssignableFrom(p.PropertyType))
 				);
 
-		private static IEnumerable<GetStateDelegate> GetStateDelegatesForType(Type type)
+		private static IEnumerable<GetStateChangedPropertyDelegate> GetStateChangedNotifierPropertyDelegatesForType(Type type)
 		{
 			return ValueDelegatesByType.GetOrAdd(type, _ =>
 			{
-				var delegates = new List<GetStateDelegate>();
-				IEnumerable<PropertyInfo> stateProperties = GetStateProperties(type);
+				var delegates = new List<GetStateChangedPropertyDelegate>();
+				IEnumerable<PropertyInfo> stateChangedNotifierProperties = GetStateChangedNotifierProperties(type);
 
-				foreach (PropertyInfo currentProperty in stateProperties)
+				foreach (PropertyInfo currentProperty in stateChangedNotifierProperties)
 				{
-					Type stateType = currentProperty.PropertyType.GetGenericArguments()[0];
-					Type iStateType = typeof(IState<>).MakeGenericType(stateType);
-					Type getterMethod = typeof(Func<,>).MakeGenericType(type, iStateType);
+					Type getterMethod = typeof(Func<,>).MakeGenericType(type, currentProperty.PropertyType);
 					var stronglyTypedDelegate = Delegate.CreateDelegate(getterMethod, currentProperty.GetGetMethod(true));
-					var getValueDelegate = new GetStateDelegate(x => (IStateChangedNotifier)stronglyTypedDelegate.DynamicInvoke(x));
+					var getValueDelegate = new GetStateChangedPropertyDelegate(
+						x => (IStateChangedNotifier)stronglyTypedDelegate.DynamicInvoke(x));
 					delegates.Add(getValueDelegate);
 				}
 
