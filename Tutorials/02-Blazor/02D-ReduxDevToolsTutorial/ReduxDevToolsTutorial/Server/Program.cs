b2b9145d45def8f0494eb67a7b6d2dc7d14a88fe
Peter Morris <mrpmorris@gmail.com>
2022-03-31 10:24:21 +0100

    Revert "Revert "Revert "Migrate to Blazor 6"""
    This reverts commit 698ce19bffd9379478d89a36a000062cd64d77b5.

--- Tutorials/02-Blazor/02D-ReduxDevToolsTutorial/ReduxDevToolsTutorial/Server/Program.cs
+++ Tutorials/02-Blazor/02D-ReduxDevToolsTutorial/ReduxDevToolsTutorial/Server/Program.cs
@@ -1,34 +1,44 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+﻿namespace FluxorBlazorWeb.ReduxDevToolsTutorial.Server
 {
-	app.UseWebAssemblyDebugging();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllersWithViews();
+			builder.Services.AddRazorPages();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseWebAssemblyDebugging();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseBlazorFrameworkFiles();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+
+			app.MapRazorPages();
+			app.MapControllers();
+			app.MapFallbackToFile("index.html");
+
+			app.Run();
+		}
+	}
 }
-else
-{
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
