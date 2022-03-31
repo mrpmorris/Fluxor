698ce19bffd9379478d89a36a000062cd64d77b5
Peter Morris <mrpmorris@gmail.com>
2022-03-31 10:18:00 +0100

    Revert "Revert "Migrate to Blazor 6""
    This reverts commit 2482b18964c7ed2e1dabc7a1a5d8ebab6a786c35.

--- Tutorials/02-Blazor/02D-ReduxDevToolsTutorial/ReduxDevToolsTutorial/Client/Program.cs
+++ Tutorials/02-Blazor/02D-ReduxDevToolsTutorial/ReduxDevToolsTutorial/Client/Program.cs
@@ -1,53 +1,49 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
+using Fluxor;
+using FluxorBlazorWeb.ReduxDevToolsTutorial.Client;
 
-namespace FluxorBlazorWeb.ReduxDevToolsTutorial.Client
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+builder.Services.AddFluxor(o =>
 {
-	public class Program
-	{
-		public static async Task Main(string[] args)
-		{
-			var builder = WebAssemblyHostBuilder.CreateDefault(args);
-			builder.RootComponents.Add<App>("#app");
-			builder.RootComponents.Add<HeadOutlet>("head::after");
-
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-			builder.Services.AddFluxor(o =>
-			{
-				o.ScanAssemblies(typeof(Program).Assembly);
+	o.ScanAssemblies(typeof(Program).Assembly);
 #if DEBUG
-				o.UseReduxDevTools(rdt =>
+	o.UseReduxDevTools(rdt =>
+	{
+		rdt.Name = "Fluxor ReduxDevTools sample";
+		rdt.StackTraceLimit = 2;
+
+		// Example of using Newtonsoft, and optionally providing serialization settings
+		rdt.UseNewtonsoftJson(_ =>
+				new Newtonsoft.Json.JsonSerializerSettings
 				{
-					rdt.Name = "Fluxor ReduxDevTools sample";
-					rdt.StackTraceLimit = 2;
+					ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver
+					{
+						NamingStrategy = new Newtonsoft.Json.Serialization.CamelCaseNamingStrategy()
+					}
+				}
+		);
 
-					// Example of using Newtonsoft, and optionally providing serialization settings
-					rdt.UseNewtonsoftJson(_ =>
-							new Newtonsoft.Json.JsonSerializerSettings
-							{
-								ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver
-								{
-									NamingStrategy = new Newtonsoft.Json.Serialization.CamelCaseNamingStrategy()
-								}
-							}
-					);
-
-					// Example of using System.Text.Json, and optionally providing serialization options
-					// Note: This won't work if RoutingMiddleware is enabled, because
-					// System.Text.Json doesn't deserialize classes without a parameterless constructor
-					// in .net 3.1
-					//rdt.UseSystemTextJson(_ =>
-					//	new System.Text.Json.JsonSerializerOptions
-					//	{
-					//		PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase
-					//	}
-					//);
-				});
+		// Example of using System.Text.Json, and optionally providing serialization options
+		// Note: This won't work if RoutingMiddleware is enabled, because
+		// System.Text.Json doesn't deserialize classes without a parameterless constructor
+		// in .net 3.1
+		//rdt.UseSystemTextJson(_ =>
+		//	new System.Text.Json.JsonSerializerOptions
+		//	{
+		//		PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase
+		//	}
+		//);
+	});
 #endif
-			});
-			await builder.Build().RunAsync();
-		}
-	}
-}
+});
+
+
+
+await builder.Build().RunAsync();
--- Tutorials/02-Blazor/02D-ReduxDevToolsTutorial/ReduxDevToolsTutorial/Server/Program.cs
+++ Tutorials/02-Blazor/02D-ReduxDevToolsTutorial/ReduxDevToolsTutorial/Server/Program.cs
@@ -1,44 +1,34 @@
-﻿namespace FluxorBlazorWeb.ReduxDevToolsTutorial.Server
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-
-			var builder = WebApplication.CreateBuilder(args);
-
-			// Add services to the container.
-
-			builder.Services.AddControllersWithViews();
-			builder.Services.AddRazorPages();
-
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseWebAssemblyDebugging();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseBlazorFrameworkFiles();
-			app.UseStaticFiles();
-
-			app.UseRouting();
-
-
-			app.MapRazorPages();
-			app.MapControllers();
-			app.MapFallbackToFile("index.html");
-
-			app.Run();
-		}
-	}
+	app.UseWebAssemblyDebugging();
 }
+else
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
