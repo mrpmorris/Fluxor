33222986b37c9cfb084193c706de59f37f06cada
Peter Morris <mrpmorris@gmail.com>
2022-09-26 18:18:47 +0100

    Fix broken tests

---
+++ Source/Tests/Fluxor.UnitTests/FeatureTests/SupportFiles/SomeFeature.cs
@@ -0,0 +1,9 @@
+﻿namespace Fluxor.UnitTests.FeatureTests.SupportFiles
+{
+	public class SomeFeature : Feature<TestState>
+	{
+		public override string GetName() => "SomeName";
+
+		protected override TestState GetInitialState() => new TestState(1);
+	}
+}
