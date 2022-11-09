d969736c04f6b53031ae70709f0acbc226baf5f5
Peter Morris <mrpmorris@gmail.com>
2022-11-09 20:22:32 +0000

    Ensure StateSelection unsubscribes proprerly - Fixes #353

---
+++ Source/Tests/Fluxor.UnitTests/StateSubscriberTests/SubscribeTests.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+namespace Fluxor.UnitTests.StateSubscriberTests
+{
+	public class SubscribeTests
+	{
+		private readonly Mock<IFeature<string>> MockFeature;
+
+		private int CallbackInvocationCount;
+		private string State = "X";
+		private IStateSelection<string, char?> Selection { get; }
+
+		[Fact]
+		public void WhenSubscribedToSubject_ThenNotificationsShouldBeReceived()
+		{
+			int invocationCount = 0;
+			_ = StateSubscriber.Subscribe(this, _ => invocationCount++);
+
+			State = "Y";
+			MockFeature.Raise(x => x.StateChanged += null, EventArgs.Empty);
+			State = "Z";
+			MockFeature.Raise(x => x.StateChanged += null, EventArgs.Empty);
+
+			Assert.Equal(2, invocationCount);
+			Assert.Equal(2, CallbackInvocationCount);
+		}
+
+		[Fact]
+		public void WhenSubscriptionIsDisposed_ThenNotificationsShouldBeReceived()
+		{
+			int invocationCount = 0;
+			using (StateSubscriber.Subscribe(this, _ => invocationCount++))
+			{
+				State = "Y";
+				MockFeature.Raise(x => x.StateChanged += null, EventArgs.Empty);
+			}
+			State = "Z";
+			MockFeature.Raise(x => x.StateChanged += null, EventArgs.Empty);
+
+			Assert.Equal(1, invocationCount);
+			Assert.Equal(1, CallbackInvocationCount);
+		}
+
+		public SubscribeTests()
+		{
+			MockFeature = new Mock<IFeature<string>>();
+			MockFeature.SetupGet(x => x.State).Returns(() => State);
+			Selection = new StateSelection<string, char?>(MockFeature.Object);
+			Selection.Select(x => x[0], valueEquals: null, selectedValueChanged: _ => CallbackInvocationCount++);
+		}
+	}
+}
