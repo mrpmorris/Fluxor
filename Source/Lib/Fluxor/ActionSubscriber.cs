3b8293a9c9ed865860d1b69d2ec63e1e41721b6c
sallyjwhite <sally@ablyss.co.uk>
2022-12-09 10:24:15 +0000

    removed spaces

--- Source/Lib/Fluxor/ActionSubscriber.cs
+++ Source/Lib/Fluxor/ActionSubscriber.cs
@@ -4,101 +4,102 @@ using System.Linq;
 
 namespace Fluxor
 {
-    internal class ActionSubscriber : IActionSubscriber
-    {
-        private readonly object SyncRoot = new();
-        private readonly Dictionary<object, List<ActionSubscription>> SubscriptionsForInstance = new();
-        private readonly Dictionary<Type, List<ActionSubscription>> SubscriptionsForType = new();
+	internal class ActionSubscriber : IActionSubscriber
+	{
+		private readonly object SyncRoot = new();
+		private readonly Dictionary<object, List<ActionSubscription>> SubscriptionsForInstance = new();
+		private readonly Dictionary<Type, List<ActionSubscription>> SubscriptionsForType = new();
 
-        public IDisposable GetActionUnsubscriberAsIDisposable(object subscriber) =>
-            new DisposableCallback(
-                id: $"{nameof(ActionSubscriber)}.{nameof(GetActionUnsubscriberAsIDisposable)}",
-                action: () => UnsubscribeFromAllActions(subscriber));
 
-        public void Notify(object action)
-        {
-            if (action is null)
-                throw new ArgumentNullException(nameof(action));
+		public IDisposable GetActionUnsubscriberAsIDisposable(object subscriber) =>
+			new DisposableCallback(
+				id: $"{nameof(ActionSubscriber)}.{nameof(GetActionUnsubscriberAsIDisposable)}",
+				action: () => UnsubscribeFromAllActions(subscriber));
 
-            lock (SyncRoot)
-            {
-                IEnumerable<Action<object>> callbacks =
-                    SubscriptionsForType
-                        .Where(x => x.Key.IsAssignableFrom(action.GetType()))
-                        .SelectMany(x => x.Value)
-                        .Select(x => x.Callback)
-                        .ToArray();
-                foreach (Action<object> callback in callbacks)
-                    callback(action);
-            }
-        }
+		public void Notify(object action)
+		{
+			if (action is null)
+				throw new ArgumentNullException(nameof(action));
 
-        public void SubscribeToAction<TAction>(object subscriber, Action<TAction> callback)
-        {
-            if (subscriber is null)
-                throw new ArgumentNullException(nameof(subscriber));
-            if (callback is null)
-                throw new ArgumentNullException(nameof(callback));
+			lock (SyncRoot)
+			{
+				IEnumerable<Action<object>> callbacks =
+					SubscriptionsForType
+						.Where(x => x.Key.IsAssignableFrom(action.GetType()))
+						.SelectMany(x => x.Value)
+						.Select(x => x.Callback)
+						.ToArray();
+				foreach (Action<object> callback in callbacks)
+					callback(action);
+			}
+		}
 
-            var subscription = new ActionSubscription(
-                subscriber: subscriber,
-                actionType: typeof(TAction),
-                callback: (object action) => callback((TAction)action));
+		public void SubscribeToAction<TAction>(object subscriber, Action<TAction> callback)
+		{
+			if (subscriber is null)
+				throw new ArgumentNullException(nameof(subscriber));
+			if (callback is null)
+				throw new ArgumentNullException(nameof(callback));
 
-            lock (SyncRoot)
-            {
-                if (!SubscriptionsForInstance.TryGetValue(subscriber, out List<ActionSubscription> instanceSubscriptions))
-                {
-                    instanceSubscriptions = new List<ActionSubscription>();
-                    SubscriptionsForInstance[subscriber] = instanceSubscriptions;
-                }
-                instanceSubscriptions.Add(subscription);
+			var subscription = new ActionSubscription(
+				subscriber: subscriber,
+				actionType: typeof(TAction),
+				callback: (object action) => callback((TAction)action));
 
-                if (!SubscriptionsForType.TryGetValue(typeof(TAction), out List<ActionSubscription> typeSubscriptions))
-                {
-                    typeSubscriptions = new List<ActionSubscription>();
-                    SubscriptionsForType[typeof(TAction)] = typeSubscriptions;
-                }
-                typeSubscriptions.Add(subscription);
-            };
-        }
+			lock (SyncRoot)
+			{
+				if (!SubscriptionsForInstance.TryGetValue(subscriber, out List<ActionSubscription> instanceSubscriptions))
+				{
+					instanceSubscriptions = new List<ActionSubscription>();
+					SubscriptionsForInstance[subscriber] = instanceSubscriptions;
+				}
+				instanceSubscriptions.Add(subscription);
 
-        public void UnsubscribeFromAllActions(object subscriber)
-        {
-            if (subscriber is null)
-                throw new ArgumentNullException(nameof(subscriber));
+				if (!SubscriptionsForType.TryGetValue(typeof(TAction), out List<ActionSubscription> typeSubscriptions))
+				{
+					typeSubscriptions = new List<ActionSubscription>();
+					SubscriptionsForType[typeof(TAction)] = typeSubscriptions;
+				}
+				typeSubscriptions.Add(subscription);
+			};
+		}
 
-            List<ActionSubscription> instanceSubscriptions;
-            lock (SyncRoot)
-            {
-                if (!SubscriptionsForInstance.TryGetValue(subscriber, out instanceSubscriptions))
-                    return;
+		public void UnsubscribeFromAllActions(object subscriber)
+		{
+			if (subscriber is null)
+				throw new ArgumentNullException(nameof(subscriber));
 
-                IEnumerable<object> subscribedInstances =
-                    instanceSubscriptions
-                    .Select(x => x.Subscriber)
-                    .Distinct();
+			List<ActionSubscription> instanceSubscriptions;
+			lock (SyncRoot)
+			{
+				if (!SubscriptionsForInstance.TryGetValue(subscriber, out instanceSubscriptions))
+					return;
 
-                IEnumerable<Type> subscribedActionTypes =
-                    instanceSubscriptions
-                        .Select(x => x.ActionType)
-                        .Distinct();
+				IEnumerable<object> subscribedInstances =
+					instanceSubscriptions
+					.Select(x => x.Subscriber)
+					.Distinct();
 
-                foreach (Type actionType in subscribedActionTypes)
-                {
-                    List<ActionSubscription> actionTypeSubscriptions;
-                    if (!SubscriptionsForType.TryGetValue(actionType, out actionTypeSubscriptions))
-                        continue;
-                    SubscriptionsForType[actionType] = actionTypeSubscriptions
-                        .Except(instanceSubscriptions)
-                        .ToList();
-                }
+				IEnumerable<Type> subscribedActionTypes =
+					instanceSubscriptions
+						.Select(x => x.ActionType)
+						.Distinct();
 
-                foreach (object subscription in subscribedInstances)
-                {
-                    SubscriptionsForInstance.Remove(subscription);
-                }
-            }
-        }
-    }
+				foreach (Type actionType in subscribedActionTypes)
+				{
+					List<ActionSubscription> actionTypeSubscriptions;
+					if (!SubscriptionsForType.TryGetValue(actionType, out actionTypeSubscriptions))
+						continue;
+					SubscriptionsForType[actionType] = actionTypeSubscriptions
+						.Except(instanceSubscriptions)
+						.ToList();
+				}
+
+				foreach (object subscription in subscribedInstances)
+				{
+					SubscriptionsForInstance.Remove(subscription);
+				}
+			}
+		}
+	}
 }
