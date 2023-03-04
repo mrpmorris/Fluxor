d391ca4eca798748cbc2b4ecad5ac00c71c4f77b
Peter Morris <mrpmorris@gmail.com>
2023-03-04 23:36:01 +0000

    Unit test to reproduce

---
+++ Source/Tests/Fluxor.UnitTests/StoreTests/ThreadingTests/DispatchReentrancyTests/SupportFiles/AutoIncrementMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fluxor.UnitTests.StoreTests.ThreadingTests.DispatchReentrancyTests.SupportFiles
+{
+	public class AutoIncrementMiddleware : Middleware
+	{
+		private IDispatcher Dispatcher;
+
+		public override Task InitializeAsync(IDispatcher dispatcher, IStore store)
+		{
+			Dispatcher = dispatcher;
+			return base.InitializeAsync(dispatcher, store);
+		}
+
+		public override void AfterDispatch(object action)
+		{
+			if (action is StoreInitializedAction)
+			{
+
+			}
+		}
+	}
+}
