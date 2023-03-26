722fc0ff9a630f1717890abf00f653d9185e50cc
Peter Morris <mrpmorris@gmail.com>
2023-03-26 14:06:44 +0100

    Hard code store classes instead of using Moxy

--- Source/Tests/Fluxor.Benchmarks/App.razor.cs
+++ Source/Tests/Fluxor.Benchmarks/App.razor.cs
@@ -1,41 +1,28 @@
-using System.Diagnostics;
+using Fluxor.Benchmarks.Benchmarks;
 
 namespace Fluxor.Benchmarks;
 
-[CreateStoreClasses]
 public partial class App
 {
 	private readonly List<string> Results = new();
 
 	private async Task RunBenchmarksAsync()
 	{
-		Results.Add("Started scan");
-		StateHasChanged();
-		await Task.Yield();
-		
-		var services = new ServiceCollection();
-		
-		var stopwatch = Stopwatch.StartNew();
-		services.AddFluxor(x => x.ScanAssemblies(typeof(App).Assembly));
-		stopwatch.Stop();
-
-		Results.Add($"Scan took {stopwatch.ElapsedMilliseconds} ms");
-		StateHasChanged();
-		await Task.Yield();
-
-		Results.Add("Starting InitializeAsync");
-		StateHasChanged();
-		await Task.Yield();
-
-		IServiceProvider serviceProvider = services.BuildServiceProvider();
-		IDispatcher dispatcher = serviceProvider.GetService<IDispatcher>();
-		IStore store = serviceProvider.GetService<IStore>();
-
-		stopwatch.Restart();
-		await store.InitializeAsync();
-		stopwatch.Stop();
-
-		Results.Add($"InitializeAsync took {stopwatch.ElapsedMilliseconds} ms");
+		await LogOutput("Running");
+		for (int i = 0; i < 5; i++)
+		{
+			await ScanTypesBenchmark.ExecuteAsync(LogOutput);
+			await ScanAssembliesBenchmark.ExecuteAsync(LogOutput);
+		}
 	}
+
+	private async Task LogOutput(string text)
+	{
+		Results.Add(text);
+		StateHasChanged();
+		await Task.Delay(1);
+	}
+
+
 }
 
---
+++ Source/Tests/Fluxor.Benchmarks/Benchmarks/ScanAssembliesBenchmark.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Fluxor.Benchmarks.Benchmarks;
+
+public static class ScanAssembliesBenchmark
+{
+	public async static Task ExecuteAsync(Func<string, Task> log)
+	{
+		var services = new ServiceCollection();
+
+		var stopwatch = Stopwatch.StartNew();
+		services.AddFluxor(x => x.ScanAssemblies(typeof(App).Assembly));
+		stopwatch.Stop();
+
+		await log($"ScanAssemblies took {stopwatch.ElapsedMilliseconds} ms");
+	}
+}
