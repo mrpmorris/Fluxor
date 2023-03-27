6197f973c04037eacec0264f9afb5d5bd0465037
Peter Morris <mrpmorris@gmail.com>
2023-03-27 19:04:00 +0100

    SourceGen crashes

---
+++ Source/Lib/Fluxor.PreScanningStoreBuilder/Helpers/HashCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fluxor.PreScanningStoreBuilder.Helpers
+{
+	internal static class HashCode
+	{
+		public static int Combine(params object[] objects)
+		{
+			unchecked
+			{
+				int hash = 17;
+
+				foreach (object obj in objects ?? Array.Empty<object>())
+					hash = hash * 23 + (obj?.GetHashCode() ?? 0);
+
+				return hash;
+			}
+		}
+	}
+}
