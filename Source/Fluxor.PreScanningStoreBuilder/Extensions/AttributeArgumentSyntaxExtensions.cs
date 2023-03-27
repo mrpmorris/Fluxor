8268f98ea6516116ded116a1b9f5432a4b9ddc54
Peter Morris <mrpmorris@gmail.com>
2023-03-27 18:52:37 +0100

    Start code-gen

---
+++ Source/Fluxor.PreScanningStoreBuilder/Extensions/AttributeArgumentSyntaxExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+
+namespace Fluxor.PreScanningStoreBuilder.Extensions
+{
+	internal static class AttributeArgumentSyntaxExtensions
+	{
+		public static string GetArgumentName(this AttributeArgumentSyntax argument) =>
+			argument.NameEquals is not null
+			? argument.NameEquals.Name.Identifier.ValueText
+			: argument.NameColon is not null
+			? argument.NameColon.Name.Identifier.ValueText
+			: throw new InvalidOperationException("Cannot find argument value");
+	}
+}
---
+++ Source/Fluxor.PreScanningStoreBuilder/SourceGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Fluxor.PreScanningStoreBuilder.SourceGenerators;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Fluxor.PreScanningStoreBuilder
+{
+	[Generator]
+	public class SourceGenerator : IIncrementalGenerator
+	{
+		public void Initialize(IncrementalGeneratorInitializationContext context)
+		{
+			//IncrementalValuesProvider<GeneratorSyntaxContext> classDeclarationSyntaxes = SelectTypeDeclarationSyntaxes(context);
+			//FeatureStateSourceBuilder.Build(context, classDeclarationSyntaxes);
+		}
+
+		//private static IncrementalValuesProvider<GeneratorSyntaxContext> SelectTypeDeclarationSyntaxes(IncrementalGeneratorInitializationContext context) =>
+		//	context.SyntaxProvider.CreateSyntaxProvider(
+		//		predicate: static (syntaxNode, _) => syntaxNode is TypeDeclarationSyntax typeDeclaration && typeDeclaration.Kind() == SyntaxKind.ClassDeclaration,
+		//		transform: static (syntaxContext, _) => syntaxContext);
+	}
+}
