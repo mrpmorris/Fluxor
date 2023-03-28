32d2f59228afe5904ad9c3f89e2ba297ba1e3e3a
Peter Morris <mrpmorris@gmail.com>
2023-03-28 09:46:59 +0100

    Test

---
+++ Source/Lib/Fluxor.StoreBuilderSourceGenerator/ModelSelectors/ClassesSelector.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Fluxor.StoreBuilderSourceGenerator.ModelSelectors;
+
+public static class ClassesSelector
+{
+	public static IncrementalValuesProvider<GeneratorSyntaxContext> Select(IncrementalGeneratorInitializationContext context) =>
+		context.SyntaxProvider.CreateSyntaxProvider(
+			predicate: static (syntaxNode, _) => syntaxNode is TypeDeclarationSyntax typeDeclaration && typeDeclaration.Kind() == SyntaxKind.ClassDeclaration,
+			transform: static (syntaxContext, _) => syntaxContext);
+}
---
+++ Source/Lib/Fluxor.StoreBuilderSourceGenerator/SourceGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Fluxor.StoreBuilderSourceGenerator.ModelSelectors;
+using Microsoft.CodeAnalysis;
+using System;
+
+namespace Fluxor.StoreBuilderSourceGenerator;
+
+[Generator]
+public class SourceGenerator : IIncrementalGenerator
+{
+	public void Initialize(IncrementalGeneratorInitializationContext context)
+	{
+		
+		IncrementalValuesProvider<GeneratorSyntaxContext> classDeclarationSyntaxes =
+			ClassesSelector.Select(context);
+
+		context.RegisterSourceOutput(
+			classDeclarationSyntaxes,
+			static (sourceProductionContext, generatorSyntaxContext) =>
+			{
+				sourceProductionContext.AddSource(
+					hintName: $"Hello.g.cs",
+					source: "//haha");
+			});
+	}
+}
