b2a352bc6072cedfc659ff7dfa4711501c7a1321
Peter Morris <mrpmorris@gmail.com>
2023-03-28 09:46:59 +0100

    Test

---
+++ Source/Lib/Fluxor.StoreBuilderSourceGenerator/ModelSelectors/FeatureStateClassesSelector.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxor.StoreBuilderSourceGenerator.ModelSelectors;
+
+internal static class FeatureStateClassesSelector
+{
+	//public static IncrementalValueProvider<string> Select(IncrementalValueProvider<GeneratorSyntaxContext> source) =>
+	//	source
+	//		.Select(x => x.SemanticModel.)
+}
