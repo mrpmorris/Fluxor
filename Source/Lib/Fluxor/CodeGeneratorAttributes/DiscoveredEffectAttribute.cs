7c18d8600d1ef76a5fcc57f36ed1c2bbeae2e9ea
Peter Morris <mrpmorris@gmail.com>
2023-05-06 13:33:21 +0100

    DiscoveredEffectAttribute

---
+++ Source/Lib/Fluxor/CodeGeneratorAttributes/DiscoveredEffectAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fluxor.CodeGeneratorAttributes;
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+public sealed class DiscoveredEffectAttribute : Attribute
+{
+	public Type Effect { get; }
+	public Type ImplementingClass { get; }
+
+	public DiscoveredEffectAttribute(Type effect, Type implementingClass)
+	{
+		Effect = effect ?? throw new ArgumentNullException(nameof(effect));
+	}
+}
