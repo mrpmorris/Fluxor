3c32f9b70e444a305280500ee6d80ba92acc61f4
Peter Morris <mrpmorris@gmail.com>
2023-05-06 12:33:07 +0100

    Add attribute to assembly for each discovered Feature<T>

---
+++ Source/Lib/Fluxor/CodeGeneratorAttributes/DiscoveredFeatureAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fluxor.CodeGeneratorAttributes;
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+public sealed class DiscoveredFeatureAttribute : Attribute
+{
+	public Type Feature { get; }
+
+	public DiscoveredFeatureAttribute(Type feature)
+	{
+		Feature = feature ?? throw new ArgumentNullException(nameof(feature));
+	}
+}
