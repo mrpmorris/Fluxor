e7c745284e8a516635f05f56f7a769834cd8bb8f
Peter Morris <mrpmorris@gmail.com>
2023-05-07 11:38:31 +0100

    Correctly call static handlers.

--- Source/Tests/Fluxor.StoreBuilderSourceGeneratorTests/Fluxor.StoreBuilderSourceGeneratorTests/TestState1.cs
+++ Source/Tests/Fluxor.StoreBuilderSourceGeneratorTests/Fluxor.StoreBuilderSourceGeneratorTests/TestState1.cs
@@ -22,14 +22,13 @@ internal class State1Reducers
 internal class State1Effects
 {
 	[EffectMethod(typeof(TestState1Action1))]
-	public static Task ExplicitlyDefinedActionHandlerAsync(IDispatcher dispatcher) => Task.CompletedTask;
+	public Task ExplicitlyDefinedActionHandlerAsync(IDispatcher dispatcher) => Task.CompletedTask;
 
 	[EffectMethod]
 	public Task ActionHandlerAsync(TestState1Action1 action, IDispatcher dispatcher) => Task.CompletedTask;
 
 	[EffectMethod]
 	public static Task StaticActionHandlerAsync(TestState1Action1 action, IDispatcher dispatcher) => Task.CompletedTask;
-
 }
 
 
