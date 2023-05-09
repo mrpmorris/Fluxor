0c09496012d113cc0fba10da80a78bd0c8dcd3ed
Peter Morris <mrpmorris@gmail.com>
2023-05-09 10:11:15 +0100

    Find reducer classes

--- Source/Lib/Fluxor.StoreBuilderSourceGenerator/ReducerClassesSelector.cs
+++ Source/Lib/Fluxor.StoreBuilderSourceGenerator/ReducerClassesSelector.cs
@@ -1,6 +1,7 @@
 ﻿using Fluxor.StoreBuilderSourceGenerator.Helpers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 
@@ -12,7 +13,7 @@ internal static class ReducerClassesSelector
 	{
 		IncrementalValueProvider<INamedTypeSymbol> fluxorIMiddlewareTypeProvider = context.CompilationProvider.Select(
 			static (Compilation compilation, CancellationToken cancellationToken) =>
-				compilation.GetTypeByMetadataName("Fluxor.IReducer<TState>"));
+				compilation.GetTypeByMetadataName("Fluxor.IReducer`1"));
 
 		var classSymbols = context
 			.SyntaxProvider
@@ -24,9 +25,21 @@ internal static class ReducerClassesSelector
 
 		IncrementalValuesProvider<string> provider = classSymbols
 			.Combine(fluxorIMiddlewareTypeProvider)
-			.Where(static x => x.Left.AllInterfaces.Contains(x.Right))
+			.Where(static x => ImplementsGenericInterface(x.Left.AllInterfaces, x.Right))
 			.Select(static (x, cancellationToken) => NamespaceHelper.Combine(x.Left.ContainingNamespace.ToDisplayString(), x.Left.Name));
 
 		return provider;
+	}
+
+	private static bool ImplementsGenericInterface(ImmutableArray<INamedTypeSymbol> allInterfaces, INamedTypeSymbol interfaceSymbol)
+	{
+		for (int i = 0; i < allInterfaces.Length; i++)
+		{
+			var implementedInterface = allInterfaces[i];
+			if (implementedInterface.OriginalDefinition.Equals(interfaceSymbol, SymbolEqualityComparer.Default))
+				return true;
+		}
+
+		return false;
 	}
 }
--- Source/Tests/Fluxor.StoreBuilderSourceGeneratorTests/Fluxor.StoreBuilderSourceGeneratorTests/TestState3.cs
+++ Source/Tests/Fluxor.StoreBuilderSourceGeneratorTests/Fluxor.StoreBuilderSourceGeneratorTests/TestState3.cs
@@ -4,22 +4,22 @@
 	{
 	}
 
-	internal class TestState3Feature : Feature<TestState3>
+	internal readonly record struct TestState3Action1();
+
+	//internal class TestState3Feature : Feature<TestState3>
+	//{
+	//	public override string GetName() => "TestState3";
+	//	protected override TestState3 GetInitialState() => new TestState3();
+	//}
+
+	internal class TestState3Action1Reducer : Reducer<TestState3, TestState3Action1>
 	{
-		public override string GetName() => "TestState3";
-		protected override TestState3 GetInitialState() => new TestState3();
+		public override TestState3 Reduce(TestState3 state, TestState3Action1 action) => state;
 	}
 
-	internal class TestState3Action1 { }
-
-	internal class TestState3Action1Reducer : Reducer<TestState3, TestState1Action1>
-	{
-		public override TestState3 Reduce(TestState3 state, TestState1Action1 action) => state;
-	}
-
-	internal class TestState3Action1Effect : Effect<TestState3Action1>
-	{
-		public override Task HandleAsync(TestState3Action1 action, IDispatcher dispatcher) => Task.CompletedTask;
-	}
+	//internal class TestState3Action1Effect : Effect<TestState3Action1>
+	//{
+	//	public override Task HandleAsync(TestState3Action1 action, IDispatcher dispatcher) => Task.CompletedTask;
+	//}
 
 }
