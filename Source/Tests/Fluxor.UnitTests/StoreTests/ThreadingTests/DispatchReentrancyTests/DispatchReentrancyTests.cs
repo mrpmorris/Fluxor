b476a4cb35772cf079756c9445a7917101216c08
Peter Morris <mrpmorris@gmail.com>
2023-05-20 10:11:45 +0100

    Fix deadlocks - Closes #407 (#422)
    * Fixes potential for deadlock ([#407](https://github.com/mrpmorris/Fluxor/issues/407))

---
+++ Source/Tests/Fluxor.UnitTests/StoreTests/ThreadingTests/DispatchReentrancyTests/DispatchReentrancyTests.cs
@@ -0,0 +1,53 @@
+﻿using Fluxor.UnitTests.StoreTests.ThreadingTests.DispatchReentrancyTests.SupportFiles;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fluxor.UnitTests.StoreTests.ThreadingTests.DispatchReentrancyTests
+{
+	public class DispatchReentrancyTests
+	{
+		private readonly IDispatcher Dispatcher;
+		private readonly IStore Subject;
+		private readonly IFeature<CounterState> Feature;
+
+		[Fact]
+		public async Task WhenObserverSubscribesToAnAction_AndDispatchesAnActionFromANewThread_ThenThereShouldBeNoDeadlock()
+		{
+			Thread initialThread = Thread.CurrentThread;
+			Subject.SubscribeToAction<StoreInitializedAction>(this, _ =>
+			{
+				var thread = new Thread(() =>
+				{
+					Thread.Sleep(50);
+					while (Thread.CurrentThread == initialThread)
+						Thread.Sleep(0);
+
+					Dispatcher.Dispatch(new IncrementCounterAction());
+				});
+				thread.Start();
+				thread.Join();
+			});
+
+			var timeout = Task.Delay(1000);
+			var initialize = Task.Run(async () =>
+			{
+				await Task.Yield();
+				await Subject.InitializeAsync();
+			});
+			await Task.WhenAny(timeout, initialize);
+			Assert.False(timeout.IsCompleted, "Time out due to deadlock");
+		}
+
+		public DispatchReentrancyTests()
+		{
+			Dispatcher = new Dispatcher();
+			Subject = new Store(Dispatcher);
+
+			Feature = new CounterFeature();
+			Subject.AddFeature(Feature);
+		}
+
+	}
+}
---
+++ Source/Tests/Fluxor.UnitTests/StoreTests/ThreadingTests/DispatchReentrancyTests/SupportFiles/CounterFeature.cs
@@ -0,0 +1,8 @@
+﻿namespace Fluxor.UnitTests.StoreTests.ThreadingTests.DispatchReentrancyTests.SupportFiles
+{
+	class CounterFeature : Feature<CounterState>
+	{
+		public override string GetName() => "Counter";
+		protected override CounterState GetInitialState() => new(counter: 0);
+	}
+}
