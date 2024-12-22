926f98bb2c5136c49756bf06c2c4e8b2990d144a
Peter Morris <mrpmorris@gmail.com>
2024-12-22 15:01:59 +0000

    Analyzer tests WIP

--- Source/Lib/Fluxor.Blazor.Web.AnalyzersUnitTests/UnitTest1.cs
+++ Source/Lib/Fluxor.Blazor.Web.AnalyzersUnitTests/UnitTest1.cs
@@ -1,94 +1,96 @@
-﻿//using System.Threading.Tasks;
-//using Xunit;
-//using VerifyCS = Microsoft.CodeAnalysis.CSharp.
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<Fluxor.Blazor.Web.Analyzers.CallBaseOnInitialized, Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
 
-//namespace Fluxor.Blazor.Web.Analyzers.Tests;
+namespace Fluxor.Blazor.Web.Analyzers.Tests;
 
-//public sealed class CallBaseOnInitializedTests
-//{
-//	[Fact]
-//	public async Task NoDiagnostic_WhenNotOverridden()
-//	{
-//		var test = @"
-//namespace Fluxor.Blazor.Web.Components
-//{
-//	public class FluxorComponent { }
-//}
-//class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
-//{
-//	void OnInitialized() { }
-//}
-//";
-//		await VerifyCS.VerifyAnalyzerAsync(source: test);
-//	}
+[TestClass]
+public sealed class CallBaseOnInitializedTests
+{
+	[TestMethod]
+	public async Task WhenNotOverridden_ThenNoDiagnosticIsEmitted()
+	{
+		string source =
+			@"""
+			namespace Fluxor.Blazor.Web.Components
+			{
+				public class FluxorComponent { }
+			}
+			class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
+			{
+				void OnInitialized() { }
+			}
+			""";
+		await VerifyCS.VerifyAnalyzerAsync(source);
+	}
 
-//	[Fact]
-//	public async Task NoDiagnostic_WhenBaseIsCalled()
-//	{
-//		var test = @"
-//namespace Fluxor.Blazor.Web.Components
-//{
-//	public class FluxorComponent { protected virtual void OnInitialized() { } }
-//}
-//class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
-//{
-//	protected override void OnInitialized()
-//	{
-//		base.OnInitialized();
-//	}
-//}
-//";
-//		await VerifyCS.VerifyAnalyzerAsync(source: test);
-//	}
+	[TestMethod]
+	public async Task NoDiagnostic_WhenBaseIsCalled()
+	{
+		string test = @"
+namespace Fluxor.Blazor.Web.Components
+{
+	public class FluxorComponent { protected virtual void OnInitialized() { } }
+}
+class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
+{
+	protected override void OnInitialized()
+	{
+		base.OnInitialized();
+	}
+}
+";
+		await VerifyCS.VerifyAnalyzerAsync(source: test);
+	}
 
-//	[Fact]
-//	public async Task Diagnostic_WhenBaseNotCalled()
-//	{
-//		var test = @"
-//namespace Fluxor.Blazor.Web.Components
-//{
-//	public class FluxorComponent { protected virtual void OnInitialized() { } }
-//}
-//class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
-//{
-//	protected override void OnInitialized()
-//	{
-//		int x = 42;
-//	}
-//}
-//";
-//		var expected = VerifyCS.Diagnostic("FLXW01").WithSpan(10, 19, 10, 32);
-//		await VerifyCS.VerifyAnalyzerAsync(source: test, expectedDiagnostics: new[] { expected });
-//	}
+	[TestMethod]
+	public async Task Diagnostic_WhenBaseNotCalled()
+	{
+		var test = @"
+namespace Fluxor.Blazor.Web.Components
+{
+	public class FluxorComponent { protected virtual void OnInitialized() { } }
+}
+class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
+{
+	protected override void OnInitialized()
+	{
+		int x = 42;
+	}
+}
+";
+		var expected = VerifyCS.Diagnostic("FLXW01").WithSpan(8, 26, 8, 39);
+		await VerifyCS.VerifyAnalyzerAsync(source: test, expected: new[] { expected });
+	}
 
-//	[Fact]
-//	public async Task NoDiagnostic_WhenNotFluxorType()
-//	{
-//		var test = @"
-//class MyBaseClass { protected virtual void OnInitialized() { } }
-//class MyComponent : MyBaseClass
-//{
-//	protected override void OnInitialized()
-//	{
-//	}
-//}
-//";
-//		await VerifyCS.VerifyAnalyzerAsync(source: test);
-//	}
+	[TestMethod]
+	public async Task NoDiagnostic_WhenNotFluxorType()
+	{
+		var test = @"
+class MyBaseClass { protected virtual void OnInitialized() { } }
+class MyComponent : MyBaseClass
+{
+	protected override void OnInitialized()
+	{
+	}
+}
+";
+		await VerifyCS.VerifyAnalyzerAsync(source: test);
+	}
 
-//	[Fact]
-//	public async Task NoDiagnostic_WhenExpressionBodyCallsBase()
-//	{
-//		var test = @"
-//namespace Fluxor.Blazor.Web.Components
-//{
-//	public class FluxorComponent { protected virtual void OnInitialized() { } }
-//}
-//class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
-//{
-//	protected override void OnInitialized() => base.OnInitialized();
-//}
-//";
-//		await VerifyCS.VerifyAnalyzerAsync(source: test);
-//	}
-//}
+	[TestMethod]
+	public async Task NoDiagnostic_WhenExpressionBodyCallsBase()
+	{
+		var test = @"
+namespace Fluxor.Blazor.Web.Components
+{
+	public class FluxorComponent { protected virtual void OnInitialized() { } }
+}
+class MyComponent : Fluxor.Blazor.Web.Components.FluxorComponent
+{
+	protected override void OnInitialized() => base.OnInitialized();
+}
+";
+		await VerifyCS.VerifyAnalyzerAsync(source: test);
+	}
+}
