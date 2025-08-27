0ee09e77a6282db822db176baf792bcf6aa965b1
Peter Morris <mrpmorris@gmail.com>
2025-08-27 14:40:55 +0100

    Recreate first Blazor demo #548

---
+++ Source/Tutorials/02-Blazor/02A-StateActionsReducersTutorial/StateActionsReducersTutorial/Store/CounterFeature/Reducers.cs
@@ -0,0 +1,10 @@
+﻿using Fluxor;
+
+namespace FluxorBlazorWeb.StateActionsReducersTutorial.Store.CounterFeature;
+
+public static class Reducers
+{
+	[ReducerMethod]
+	public static CounterState Reduce(CounterState state, IncrementCounterAction action) =>
+		new CounterState(ClickCount: state.ClickCount + 1);
+}
