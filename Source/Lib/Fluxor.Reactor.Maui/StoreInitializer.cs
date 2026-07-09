31fbf998d8192557c77e52b017c1e4a214c8aed7
Nathaniel Bond <nabond251@gmail.com>
2026-07-08 22:00:42 -0400

    Clean up init

--- Source/Lib/Fluxor.Reactor.Maui/StoreInitializer.cs
+++ Source/Lib/Fluxor.Reactor.Maui/StoreInitializer.cs
@@ -1,12 +1,7 @@
 ﻿using Fluxor.Reactor.Maui.Components;
-using Fluxor.Exceptions;
 using MauiReactor;
-using System;
 using System.Linq;
-using System.Runtime.ExceptionServices;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Maui.ApplicationModel;
 
 namespace Fluxor.Reactor.Maui;
 
@@ -17,8 +12,6 @@ public partial class StoreInitializer : FluxorComponent
 {
 	[Inject]
 	private IStore Store;
-
-	private Exception ExceptionToThrow;
 
 	public override VisualNode Render()
 	{
@@ -31,56 +24,16 @@ public partial class StoreInitializer : FluxorComponent
 	/// <param name="disposing">true if called manually, otherwise false</param>
 	protected override ValueTask DisposeAsyncCore(bool disposing)
 	{
-		if (disposing)
-			Store.UnhandledException -= OnUnhandledException;
 		return base.DisposeAsyncCore(disposing);
 	}
 
 	/// <summary>
-	/// Retrieves supporting JavaScript for any Middleware
+	/// Initializes the store
 	/// </summary>
 	protected override void OnMounted()
 	{
-		Store.UnhandledException += OnUnhandledException;
-
 		Store.InitializeAsync();
 
 		base.OnMounted();
-	}
-
-	protected override void OnPropsChanged()
-	{
-		base.OnPropsChanged();
-		OnAfterRender();
-	}
-
-	protected override void OnWillUnmount()
-	{
-		base.OnWillUnmount();
-		OnAfterRender();
-	}
-
-	private void OnAfterRender()
-	{
-		if (ExceptionToThrow is not null)
-		{
-			Exception exception = ExceptionToThrow;
-			ExceptionToThrow = null;
-			ExceptionDispatchInfo.Capture(exception).Throw();
-		}
-	}
-
-	private void OnUnhandledException(object sender, Exceptions.UnhandledExceptionEventArgs e)
-	{
-		MainThread.InvokeOnMainThreadAsync(async () =>
-		{
-			Exception exceptionThrownInHandler = e.Exception;
-
-			if (exceptionThrownInHandler is not null || !e.WasHandled)
-			{
-				ExceptionToThrow = exceptionThrownInHandler ?? e.Exception;
-				Invalidate();
-			}
-		});
 	}
 }
