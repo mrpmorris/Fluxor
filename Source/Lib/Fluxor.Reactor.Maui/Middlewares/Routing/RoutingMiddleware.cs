8f6dfdfeb77959d1245fccfaff3fdf574e65e96c
Nathaniel Bond <nabond251@gmail.com>
2026-07-10 22:19:22 -0400

    Remove routing... for now

--- Source/Lib/Fluxor.Reactor.Maui/Middlewares/Routing/RoutingMiddleware.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fluxor.Reactor.Maui.Middlewares.Routing;
-
-/// <summary>
-/// Adds support for routing <see cref="Microsoft.Maui.Controls.Shell"/>
-/// via a Fluxor store.
-/// </summary>
-internal class RoutingMiddleware : Middleware
-{
-}
