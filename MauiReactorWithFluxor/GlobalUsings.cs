3eb1ff79262dc4ff3266701221b63994382b14ae
Nathaniel Bond <nabond251@gmail.com>
2026-07-10 22:19:15 -0400

    Remove test app

--- MauiReactorWithFluxor/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.Maui;
-global using Microsoft.Maui.Hosting;
-global using Microsoft.Maui.Graphics;
-global using MauiControls = Microsoft.Maui.Controls;
-
-global using MauiReactor;
--- MauiReactorWithFluxor/Store/CounterState.cs
+++
@@ -1,4 +0,0 @@
-public record CounterState 
-{
-    public int CurrentCount { get; init; }
-}
